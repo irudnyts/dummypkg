acddf36d9b9732e01e790460a3f57ee287b70934
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2019-12-04 20:34:33 +0100

    Modify Shiny app to include calls of internal and external functions

--- inst/shiny-examples/demo/app.R
+++ inst/shiny-examples/demo/app.R
@@ -24,15 +24,9 @@ server <- function(input, output) {
 
     output$distPlot <- shiny::renderPlot({
 
-        test() # example of calling internal function
         p <- ggplot2::ggplot() # example of calling external function
+        my_function() # example of calling internal function
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
 }
 
